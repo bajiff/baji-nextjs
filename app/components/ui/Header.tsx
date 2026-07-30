4027646a2cea0a2d54936947272d1a4b82f49a64
BajiLinux <mr.baji.smart@gmail.com>
2026-07-30 21:22:09 +0700

    feat(update): Bikin barrel file, bikin Folder components/{layouts,ui} yang isi nya /ui/ Header.tsx index.js, Main.tsx, dan lanjut nanti saya kasih Footer.tsx

---
+++ app/components/ui/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <nav>
+        <div className="logo"><a href="#">Baji News</a></div>
+      </nav>
+    </header>
+  );
+}
+export default Header
